9dc0469df9948dc7724b5c60999ac4dbfe905afa
PacoDw <__pm@outlook.com>
2019-12-09 09:23:25 -0600

    chore: added organization resource

--- mongodbatlas/mongodbatlas.go
+++ mongodbatlas/mongodbatlas.go
@@ -47,6 +47,7 @@ type Client struct {
 	PrivateIPMode                    PrivateIpModeService
 	MaintenanceWindows               MaintenanceWindowsService
 	Teams                            TeamsService
+	Organizations                    OrganizationsService
 
 	onRequestCompleted RequestCompletionCallback
 }
@@ -154,6 +155,7 @@ func NewClient(httpClient *http.Client) *Client {
 	c.MaintenanceWindows = &MaintenanceWindowsServiceOp{client: c}
 	c.Teams = &TeamsServiceOp{client: c}
 
+	c.Organizations = &OrganizationsServiceOp{client: c}
 	return c
 }
 
