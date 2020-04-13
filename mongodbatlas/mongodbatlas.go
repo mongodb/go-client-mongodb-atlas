643d507b35afb19b01b69a6710d152cb64ed0bcf
Gustavo Bazan <gssbzn@gmail.com>
2020-04-13 17:08:29 +0100

    Update mongodbatlas.go

--- mongodbatlas/mongodbatlas.go
+++ mongodbatlas/mongodbatlas.go
@@ -21,7 +21,7 @@ const (
 	libraryVersion = "0.1"
 	defaultBaseURL = "https://cloud.mongodb.com/api/atlas/v1.0/"
 	userAgent      = "go-mongodbatlas" + libraryVersion
-	mediaType      = "application/json"
+	jsonMediaType  = "application/json"
 	gzipMediaType  = "application/gzip"
 )
 
@@ -275,9 +275,9 @@ func (c *Client) NewRequest(ctx context.Context, method, urlStr string, body int
 	}
 
 	if body != nil {
-		req.Header.Set("Content-Type", mediaType)
+		req.Header.Set("Content-Type", jsonMediaType)
 	}
-	req.Header.Add("Accept", mediaType)
+	req.Header.Add("Accept", jsonMediaType)
 	if c.UserAgent != "" {
 		req.Header.Set("User-Agent", c.UserAgent)
 	}
