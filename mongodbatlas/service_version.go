488da60f1b4a6d03a025d7f264562c0d3099f60a
Filipe Constantinov Menezes <filipe.menezes@mongodb.com>
2021-09-01 17:30:58 +0100

    CLOUDP-82836: Parse service version

---
+++ mongodbatlas/service_version.go
@@ -0,0 +1,44 @@
+// Copyright 2021 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mongodbatlas
+
+import (
+	"context"
+	"net/http"
+)
+
+const versionPath = "api/private/unauth/version"
+
+// ServiceVersionService is an interface for interfacing with the version private endpoint of the MongoDB Atlas API.
+type ServiceVersionService interface {
+	Get(context.Context) (*ServiceVersion, *Response, error)
+}
+
+type ServiceVersionServiceOp service
+
+// Get gets a compressed (.gz) log file that contains a range of log messages for a particular host.
+func (s *ServiceVersionServiceOp) Get(ctx context.Context) (*ServiceVersion, *Response, error) {
+	req, err := s.Client.NewRequest(ctx, http.MethodGet, versionPath, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := s.Client.Do(ctx, req, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resp.GetServiceVersion(), resp, nil
+}
