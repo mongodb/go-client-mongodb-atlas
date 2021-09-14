151291ec6779144076063a9898a98db04007e599
Abner Garcia <84406577+abner-dou@users.noreply.github.com>
2021-09-14 12:47:38 -0600

    INTMDB-211: Added shard keys properties to Managed Namespace in global configurat… (#253)

--- mongodbatlas/global_clusters.go
+++ mongodbatlas/global_clusters.go
@@ -48,9 +48,11 @@ type GlobalCluster struct {
 
 // ManagedNamespace represents the information about managed namespace configuration.
 type ManagedNamespace struct {
-	Db             string `json:"db"` //nolint:stylecheck // not changing this as is a breaking change
-	Collection     string `json:"collection"`
-	CustomShardKey string `json:"customShardKey,omitempty"`
+	Db                     string `json:"db"` //nolint:stylecheck // not changing this as is a breaking change
+	Collection             string `json:"collection"`
+	CustomShardKey         string `json:"customShardKey,omitempty"`
+	IsCustomShardKeyHashed *bool  `json:"isCustomShardKeyHashed,omitempty"` // Flag that specifies whether the custom shard key for the collection is hashed.
+	IsShardKeyUnique       *bool  `json:"isShardKeyUnique,omitempty"`       // Flag that specifies whether the underlying index enforces a unique constraint.
 }
 
 // CustomZoneMappingsRequest represents the request related to add custom zone mappings to a global cluster.
