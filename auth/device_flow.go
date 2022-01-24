47056f2de65281e04b6adc6e53f1be9d21fa1534
Gustavo Bazan <gus.bazan@hey.com>
2022-01-24 18:47:07 +0000

    CLOUDP-110641: Add device grant to atlas client

---
+++ auth/device_flow.go
@@ -0,0 +1,99 @@
+// Copyright 2022 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	atlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+// DeviceCode holds information about the authorization-in-progress.
+type DeviceCode struct {
+	UserCode        string `json:"user_code"`        // UserCode is the code presented to users
+	VerificationURI string `json:"verification_uri"` // VerificationURI is the URI where users will need to confirm the code
+	DeviceCode      string `json:"device_code"`      // DeviceCode is the internal code to confirm the status of the flow
+	ExpiresIn       int    `json:"expires_in"`       // ExpiresIn when the code will expire
+	Interval        int    `json:"interval"`         // Interval who often to verify the status of the code
+
+	timeNow   func() time.Time
+	timeSleep func(time.Duration)
+}
+
+// RequestCode initiates the authorization flow by requesting a code.
+func (c Config) RequestCode(ctx context.Context) (*DeviceCode, *atlas.Response, error) {
+	req, err := c.NewRequest(ctx, http.MethodPost, "api/private/unauth/account/device/authorize",
+		url.Values{
+			"client_id": {c.ClientID},
+			"scope":     {strings.Join(c.Scopes, " ")},
+		},
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	var r *DeviceCode
+	resp, err2 := c.Do(ctx, req, &r)
+	return r, resp, err2
+}
+
+// ErrTimeout is thrown when polling the server for the granted token has timed out.
+var ErrTimeout = errors.New("authentication timed out")
+
+// PollToken polls the server until an access token is granted or denied.
+func (c Config) PollToken(ctx context.Context, code *DeviceCode) (*Token, *atlas.Response, error) {
+	timeNow := code.timeNow
+	if timeNow == nil {
+		timeNow = time.Now
+	}
+	timeSleep := code.timeSleep
+	if timeSleep == nil {
+		timeSleep = time.Sleep
+	}
+
+	checkInterval := time.Duration(code.Interval) * time.Second
+	expiresAt := timeNow().Add(time.Duration(code.ExpiresIn) * time.Second)
+	req, err := c.NewRequest(ctx, http.MethodPost, "api/private/unauth/account/device/token",
+		url.Values{
+			"client_id":   {c.ClientID},
+			"device_code": {code.DeviceCode},
+			"grant_type":  {"urn:ietf:params:oauth:grant-type:device_code"},
+		},
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	for {
+		timeSleep(checkInterval)
+		var r *Token
+		resp, err2 := c.Do(ctx, req, &r)
+		var target *atlas.ErrorResponse
+		if errors.As(err2, &target) && target.ErrorCode == "DEVICE_AUTHORIZATION_PENDING" {
+			continue
+		}
+		if err2 != nil {
+			return nil, resp, err2
+		}
+
+		if timeNow().After(expiresAt) {
+			return nil, nil, ErrTimeout
+		}
+		return r, resp, nil
+	}
+}
---
+++ auth/oauth_test.go
@@ -0,0 +1,211 @@
+// Copyright 2022 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"reflect"
+	"testing"
+
+	atlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+const (
+	// baseURLPath is a non-empty Client.BaseURL path to use during tests,
+	// to ensure relative URLs are used for all endpoints.
+	baseURLPath = "/api-v1"
+)
+
+var (
+	ctx = context.TODO()
+)
+
+// setup sets up a test HTTP server along with an auth.Config that is
+// configured to talk to that test server. Tests should register handlers on
+// mux which provide mock responses for the API method being tested.
+func setup() (config *Config, mux *http.ServeMux, teardown func()) {
+	// mux is the HTTP request multiplexer used with the test server.
+	mux = http.NewServeMux()
+
+	// We want to ensure that tests catch mistakes where the endpoint URL is
+	// specified as absolute rather than relative. It only makes a difference
+	// when there's a non-empty base URL path. So, use that.
+	apiHandler := http.NewServeMux()
+	apiHandler.Handle(baseURLPath+"/", http.StripPrefix(baseURLPath, mux))
+	apiHandler.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(os.Stderr, "FAIL: Client.BaseURL path prefix is not preserved in the request URL:")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "\t"+req.URL.String())
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "\tDid you accidentally use an absolute endpoint URL rather than relative?")
+		http.Error(w, "Client.BaseURL path prefix is not preserved in the request URL.", http.StatusInternalServerError)
+	})
+
+	// server is a test HTTP server used to provide mock API responses.
+	server := httptest.NewServer(apiHandler)
+
+	// client is the Atlas client being tested and is
+	// configured to use test server.
+	config = NewConfig(nil)
+	u, _ := url.Parse(server.URL + baseURLPath + "/")
+	config.AuthURL = u
+
+	return config, mux, server.Close
+}
+
+func testMethod(t *testing.T, r *http.Request, expected string) {
+	t.Helper()
+	if expected != r.Method {
+		t.Errorf("Request method = %v, expected %v", r.Method, expected)
+	}
+}
+
+func testClientDefaultBaseURL(t *testing.T, c *Config) {
+	t.Helper()
+	if c.AuthURL == nil || c.AuthURL.String() != defaultBaseURL {
+		t.Errorf("NewConfig BaseURL = %v, expected %v", c.AuthURL, defaultBaseURL)
+	}
+}
+
+func testClientDefaultUserAgent(t *testing.T, c *Config) {
+	t.Helper()
+	if c.UserAgent != userAgent {
+		t.Errorf("NewConfig UserAgent = %v, expected %v", c.UserAgent, userAgent)
+	}
+}
+
+func testClientDefaults(t *testing.T, c *Config) {
+	t.Helper()
+	testClientDefaultBaseURL(t, c)
+	testClientDefaultUserAgent(t, c)
+}
+
+func TestNewConfig(t *testing.T) {
+	c := NewConfig(nil)
+	testClientDefaults(t, c)
+}
+
+func TestNewConfigWithOptions(t *testing.T) {
+	c, err := NewConfigWithOptions(nil)
+
+	if err != nil {
+		t.Fatalf("NewConfigWithOptions(): %v", err)
+	}
+	testClientDefaults(t, c)
+}
+
+func TestNewRequest_withCustomUserAgent(t *testing.T) {
+	ua := fmt.Sprintf("testing/%s", atlas.Version)
+	c, err := NewConfigWithOptions(nil, SetUserAgent(ua))
+
+	if err != nil {
+		t.Fatalf("NewConfigWithOptions() unexpected error: %v", err)
+	}
+
+	req, _ := c.NewRequest(ctx, http.MethodGet, "/foo", nil)
+
+	expected := fmt.Sprintf("%s %s", ua, userAgent)
+	if got := req.Header.Get("User-Agent"); got != expected {
+		t.Errorf("NewConfigWithOptions() UserAgent = %s; expected %s", got, expected)
+	}
+}
+
+func TestNewPlainRequest(t *testing.T) {
+	c := NewConfig(nil)
+
+	requestPath := "foo"
+
+	inURL, outURL := requestPath, defaultBaseURL+requestPath
+	req, _ := c.NewRequest(ctx, http.MethodGet, inURL, nil)
+
+	// test relative URL was expanded
+	if req.URL.String() != outURL {
+		t.Errorf("NewPlainRequest(%v) URL = %v, expected %v", inURL, req.URL, outURL)
+	}
+
+	// test accept content type is correct
+	accept := req.Header.Get("Accept")
+	if accept != "application/json" {
+		t.Errorf("NewPlainRequest() Accept = %v, expected %v", accept, "application/json")
+	}
+	contentType := req.Header.Get("Content-Type")
+	if contentType != "application/x-www-form-urlencoded" {
+		t.Errorf("NewPlainRequest() Accept = %v, expected %v", contentType, "application/x-www-form-urlencoded")
+	}
+	// test default user-agent is attached to the request
+	uA := req.Header.Get("User-Agent")
+	if c.UserAgent != uA {
+		t.Errorf("NewPlainRequest() User-Agent = %v, expected %v", uA, c.UserAgent)
+	}
+}
+
+func TestDo(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
+
+	type foo struct {
+		A string
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if m := http.MethodGet; m != r.Method {
+			t.Errorf("Request method = %v, expected %v", r.Method, m)
+		}
+		fmt.Fprint(w, `{"A":"a"}`)
+	})
+
+	req, _ := client.NewRequest(ctx, http.MethodGet, ".", nil)
+	body := new(foo)
+	_, err := client.Do(context.Background(), req, body)
+	if err != nil {
+		t.Fatalf("Do(): %v", err)
+	}
+
+	expected := &foo{"a"}
+	if !reflect.DeepEqual(body, expected) {
+		t.Errorf("Response body = %v, expected %v", body, expected)
+	}
+}
+
+func TestCustomUserAgent(t *testing.T) {
+	ua := fmt.Sprintf("testing/%s", atlas.Version)
+	c, err := NewConfigWithOptions(nil, SetUserAgent(ua))
+
+	if err != nil {
+		t.Fatalf("New() unexpected error: %v", err)
+	}
+
+	expected := fmt.Sprintf("%s %s", ua, userAgent)
+	if got := c.UserAgent; got != expected {
+		t.Errorf("New() UserAgent = %s; expected %s", got, expected)
+	}
+}
+
+func TestCustomBaseURL(t *testing.T) {
+	const baseURL = "http://localhost/foo"
+	c, err := NewConfigWithOptions(nil, SetAuthURL(baseURL))
+
+	if err != nil {
+		t.Fatalf("New() unexpected error: %v", err)
+	}
+	if got := c.AuthURL.String(); got != baseURL {
+		t.Errorf("New() BaseURL = %s; expected %s", got, baseURL)
+	}
+}
