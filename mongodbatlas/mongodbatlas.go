4f7d48f30c423d7fa16ff50f2c5bd37aca3f21e6
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-01-09 16:15:21 +0000

    task: release prep for 0.21.0 (#341)

--- mongodbatlas/mongodbatlas.go
+++ mongodbatlas/mongodbatlas.go
@@ -41,7 +41,7 @@ const (
 	gzipMediaType  = "application/gzip"
 	libraryName    = "go-mongodbatlas"
 	// Version the version of the current API client. Should be set to the next version planned to be released.
-	Version = "0.20.0"
+	Version = "0.21.0"
 )
 
 var (
