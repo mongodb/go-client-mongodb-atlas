6fe362f299b7cc58cb030bac6e0fdb4dafaa29c7
wtrocki <w.trocki@mongodb.com>
2023-03-17 13:22:37 +0100

    fix: update package name

--- tools/scripts/generate.sh
+++ tools/scripts/generate.sh
@@ -15,7 +15,7 @@ OPENAPI_FILE_NAME=${OPENAPI_FILE_NAME:-atlas-api.yaml}
 SDK_FOLDER=${SDK_FOLDER:-./}
 
 transformed_file="atlas-api-transformed.yaml"
-client_package="latest"
+client_package="mongodbatlasv2"
 openapiFileLocation="$OPENAPI_FOLDER/$transformed_file"
 
 echo "# Running generation pipeline"
