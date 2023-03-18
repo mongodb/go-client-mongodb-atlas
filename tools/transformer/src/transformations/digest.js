9e3c822c6c4d8a2f1707622f32fef0cd9abf0e97
wtrocki <w.trocki@mongodb.com>
2023-03-18 17:15:50 +0100

    fix: improve for the cases

--- tools/transformer/src/transformations/digest.js
+++ tools/transformer/src/transformations/digest.js
@@ -1,14 +1,16 @@
+/**
+ * Remove digest auth from paths and security schemes
+ */
 function applyDigestTransformations(api) {
   const hasSchemas = api && api.components && api.components.securitySchemes;
   if (hasSchemas) {
     delete api.components.securitySchemes.DigestAuth;
+    Object.keys(api.paths).forEach((path) => {
+      Object.keys(api.paths[path]).forEach((method) => {
+        delete api.paths[path][method].security;
+      });
+    });
   }
-
-  Object.keys(api.paths).forEach((path) => {
-     Object.keys(api.paths[path]).forEach((method)=>{
-      delete api.paths[path][method].security
-     });
-  });
 
   return api;
 }
