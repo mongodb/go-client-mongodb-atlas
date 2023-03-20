e266c1219d5541da8f46a6340c44543db010bc86
Wojciech Trocki <w.trocki@mongodb.com>
2023-03-20 14:34:38 +0100

    documentation for tools (#414)

--- tools/transformer/src/transform.js
+++ tools/transformer/src/transform.js
@@ -1,13 +1,6 @@
-const {
-  applyAllOfTransformations,
-  applyOneOfTransformations,
-  applyModelNameTransformations,
-  applyDiscriminatorTransformations,
-} = require("./transformations");
 const { getAPI, saveAPI } = require("./engine/apifile");
-const ignoredModelNames = require("./name.ignore.json").ignoreModels;
-
 const log = require("simple-node-logger").createSimpleLogger();
+const runTransformations = require("./atlasTransformations");
 
 // Override default logger
 global.console = log;
@@ -15,13 +8,5 @@ log.setLevel(process.env.XGEN_LOGGING_LEVEL || "warn");
 
 let { doc, apiFileLocation } = getAPI(process.argv.slice(2));
 
-doc = applyDiscriminatorTransformations(doc);
-doc = applyOneOfTransformations(doc);
-doc = applyAllOfTransformations(doc);
-
-doc = applyModelNameTransformations(doc, "Api", "", ignoredModelNames);
-doc = applyModelNameTransformations(doc, "Atlas", "", ignoredModelNames);
-doc = applyModelNameTransformations(doc, "", "Manual", ignoredModelNames);
-doc = applyModelNameTransformations(doc, "", "View", ignoredModelNames);
-
+doc = runTransformations(doc);
 saveAPI(doc, apiFileLocation);
