fc9a22bb0467af34852749b7d0156a6ba2f147d0
maastha <122359335+maastha@users.noreply.github.com>
2023-05-11 15:58:28 +0100

    INTMDB-813: 0.26.0 pre-release (#474)

--- mongodbatlas/mongodbatlas.go
+++ mongodbatlas/mongodbatlas.go
@@ -41,7 +41,7 @@ const (
 	gzipMediaType  = "application/gzip"
 	libraryName    = "go-mongodbatlas"
 	// Version the version of the current API client. Should be set to the next version planned to be released.
-	Version = "0.25.0"
+	Version = "0.26.0"
 )
 
 var (
